4f5166ae43cfc0827b4b147e89ae66279a168fef
Diana <dianavaleeva@yahoo.com>
2020-10-25 14:27:30 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2) return false;
+    let symbArr = str.split('');
+
+    for (let i = 0; i < symbArr.length; i++) {
+        bracketsConfig.forEach(bracket => {
+            {
+                if (symbArr[i] === bracket[0] && symbArr[i + 1] === bracket[1]) {
+                    symbArr.splice(i, 2);
+                    i -= 2;
+                }
+            }
+        });
+    }
+    return symbArr.length === 0;
+};
